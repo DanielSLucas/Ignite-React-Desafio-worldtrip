8168369c11cd532644459a066d29685b34a8b504
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-18 17:46:22 -0300

    fix slide

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -46,19 +46,35 @@ export function Slider() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <Flex
-            flex="1"
-            h={450}
-            direction="column"
-            align="center"
-            justify="center"
+          <Box
             bgImg="https://images.unsplash.com/photo-1581195177169-790ceff67d8a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470"
             bgRepeat="no-repeat"
             bgSize="cover"
           >
-            <Heading>América do Norte</Heading>
-            <Text>O continente mais antigo</Text>
-          </Flex>
+            <Flex
+              flex="1"
+              h={450}
+              direction="column"
+              align="center"
+              justify="center"
+              bgColor="rgba(28, 20, 1, 0.35)"
+            >
+              <Heading
+                fontWeight="bold"
+                fontSize={['2xl', '5xl']}
+                color="light.headings"
+              >
+                América do Norte
+              </Heading>
+              <Text
+                fontWeight="bold"
+                fontSize={['sm', '2xl']}
+                color="light.info.500"
+              >
+                O continente mais antigo
+              </Text>
+            </Flex>
+          </Box>
         </SwiperSlide>
       </Swiper>
     </Flex>
